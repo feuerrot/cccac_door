1225f851bd687f35984473fb8852affb36ed126a
Markus Witt <feuerrot@feuerrot.org>
2016-09-30 21:02:44 +0200

    pir sensors & new door status topic
    sensors/door/movement		Exakt über der Türe
    sensors/door/movement_corridor	Bereich vor der Türe auf dem Flur
    sensors/door/open		ex. door/status

--- init.lua
+++ init.lua
@@ -1,6 +1,8 @@
 bmp180 = require("bmp180")
 
 door_open = "none"
+door_move = "none"
+door_move_corridor = "none"
 shutdown = 0
 status = "public"
 
@@ -33,15 +35,34 @@ end
 
 function gpio_init()
 	gpio.mode(0, gpio.INPUT, 1)
+	gpio.mode(5, gpio.INPUT, 0)
+	gpio.mode(6, gpio.INPUT, 0)
+end
+
+function gpio_move()
+	if (gpio.read(5) == 0 and door_move ~= 0) then
+		door_move = 0
+		m:publish("sensors/door/movement", "0", 0, 1)
+	elseif (gpio.read(5) == 1 and door_move ~= 1) then
+		door_move = 1
+		m:publish("sensors/door/movement", "1", 0, 1)
+	end
+	if (gpio.read(6) == 0 and door_move_corridor ~= 0) then
+		door_move_corridor = 0
+		m:publish("sensors/door/movement_corridor", "0", 0, 1)
+	elseif (gpio.read(6) == 1 and door_move_corridor ~= 1) then
+		door_move_corridor = 1
+		m:publish("sensors/door/movement_corridor", "1", 0, 1)
+	end
 end
 
 function gpio_door()
 	if (gpio.read(0) == 0 and door_open ~= 0) then
 		door_open = 0
-		m:publish("door/status", "close", 0, 1)
+		m:publish("sensors/door/open", "0", 0, 1)
 	elseif (gpio.read(0) == 1 and door_open ~= 1) then
 		door_open = 1
-		m:publish("door/status", "open", 0, 1)
+		m:publish("sensors/door/open", "1", 0, 1)
 	end
 end
 
@@ -52,10 +73,10 @@ end
 
 function bmp180_senddata()
 	bmp180.read(2)
-	t = bmp180.getTemperature()
-	t_t = (t/10) .. "." .. (t%10)
-	p = bmp180.getPressure()
-	p_t = (p/100) .. "." .. (p%100)
+	local t = bmp180.getTemperature()
+	local t_t = (t/10) .. "." .. (t%10)
+	local p = bmp180.getPressure()
+	local p_t = (p/100) .. "." .. (p%100)
 	m:publish("sensors/door/temperature", t_t, 0, 1)
 	m:publish("sensors/door/pressure", p_t, 0, 1)
 end
@@ -107,6 +128,7 @@ function mqtt_subscribe()
 	m:subscribe("runlevel",0, function(client) print("subscribe success: runlevel") end)
 	tmr.start(0)
 	tmr.start(1)
+	tmr.start(2)
 end
 
 function mqtt_init()
@@ -121,6 +143,7 @@ function timer_init()
 	print("timer_init()")
 	tmr.register(0, 100, tmr.ALARM_AUTO, gpio_door)
 	tmr.register(1, 10000, tmr.ALARM_AUTO, bmp180_senddata)
+	tmr.register(2, 100, tmr.ALARM_AUTO, gpio_move)
 end
 
 ws2812.init()
